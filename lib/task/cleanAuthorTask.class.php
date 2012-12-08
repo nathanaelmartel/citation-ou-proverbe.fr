dce670fa96654f9ff46d9a3a269425e00224809c
Nathanaël Martel <nathanael@fam-martel.eu>
2012-12-08 22:58:20 +0100

    clean author task

---
+++ lib/task/cleanAuthorTask.class.php
@@ -0,0 +1,86 @@
+<?php
+
+class cleanAuthorTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+      // add your own options here
+    ));
+
+    $this->namespace        = '';
+    $this->name             = 'cleanAuthor';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [cleanAuthor|INFO] task does things.
+Call it with:
+
+  [php symfony cleanAuthor|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+    
+    require_once(dirname(__FILE__).'/../vendor/simplement/scraper.class.php');
+    sfTask::log('==== begin on '.date('r').' ====');
+    
+    
+    $q = Doctrine_Query::create()
+    ->select('*')
+    ->from('Author')
+    ->where('slug LIKE ?', '%1')
+    ->limit(100)
+    ->orderBy('name');
+    
+    foreach ($q->execute() as $Author) {
+    	
+    	if (count($Author->Citations) == 0) {
+    		$Author->delete();
+    	} else {
+	    	$clean_name = scraper::cleanAuthor($Author->name);
+	    	sfTask::log($clean_name.' ('.$Author->id.') ['.count($Author->Citations).']');
+	    	
+	    	$q = Doctrine_Query::create()
+	    	->select('*')
+	    	->from('Author')
+	    	->where('slug = ?', str_replace('-1', '', $Author->slug))
+	    	->orderBy('name');
+	    	
+	    	foreach ($q->execute() as $other_author) {
+	    		if ($other_author->id != $Author->id) {
+	    			$this->changeCitationsAuthor($Author, $other_author);
+	    		}
+	    	}
+	    	
+		    $Author->name = $clean_name;
+		    $Author->save();
+    	}
+    }
+    
+    sfTask::log('==== end on '.date('r').' ====');
+  }
+  
+  function changeCitationsAuthor($OldAuthor, $NewAuthor) {
+  	
+    foreach($OldAuthor->Citations as $Citation) {
+    	$Citation->author_id = $NewAuthor->id;
+    	$Citation->save();
+    }
+    
+    //sfTask::log($OldAuthor->name.' ('.$OldAuthor->id.') -> '.$NewAuthor->name.' ('.$NewAuthor->id.') ('.count($OldAuthor->Citations).')');
+  	$OldAuthor->delete();
+  	
+  }
+}
