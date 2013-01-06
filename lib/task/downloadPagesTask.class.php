9e44f3cabf8ea564a9f3269e377ab4644d118b41
Nathanaël Martel <nathanael@fam-martel.eu>
2013-01-06 14:35:24 +0100

    scrapper

--- lib/task/downloadPagesTask.class.php
+++ lib/task/downloadPagesTask.class.php
@@ -52,7 +52,7 @@ EOF;
     	->from('Page l')
     	->where('downloaded_date is NULL')
     	->andWhere('website = ?', $website)
-    	->andWhere('http_code = ?', '')
+    	->andWhere('http_code is NULL')
     	->offset(rand(0, 50))
     	->limit(ceil(40/count($websites)))
     	->orderBy('created_at ASC');
