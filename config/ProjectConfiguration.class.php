83d120d59cb7e9c42043d25c2e25c9aec6df485c
Nathanaël Martel <nathanael@fam-martel.eu>
2013-02-23 20:47:55 +0100

    combine

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
-//require_once(dirname(__FILE__).'/../plugins/swCombinePlugin/lib/config/swCombineViewConfigHandler.class.php');
+require_once(dirname(__FILE__).'/../plugins/swCombinePlugin/lib/config/swCombineViewConfigHandler.class.php');
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
