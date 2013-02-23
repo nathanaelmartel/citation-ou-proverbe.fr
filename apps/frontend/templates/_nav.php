3652fb0d1cfbfe8d071737cf3d2c1cb53f397c90
Nathanaël Martel <nathanael@fam-martel.eu>
2013-02-23 20:32:33 +0100

    random

--- apps/frontend/templates/_nav.php
+++ apps/frontend/templates/_nav.php
@@ -4,8 +4,8 @@
 	    <ul>
 	    	<li><a href="<?php echo url_for('@homepage')?>">Dernières Citations</a></li>
 	    	<!-- <li><a href="<?php echo url_for('@authors')?>">Auteurs</a></li>
-	    	<li><a href="<?php echo url_for('@tags')?>">Thèmes</a></li>
-	    	<li><a href="<?php echo url_for('@random')?>">Une Citation Aléatoire</a></li> -->
+	    	<li><a href="<?php echo url_for('@tags')?>">Thèmes</a></li> -->
+	    	<li><a href="<?php echo url_for('@random')?>">Une Citation Aléatoire</a></li>
 	    	<li><a href="https://twitter.com/1citation" target="_blank" >Suivez nous sur Twitter</a></li>
 	    	<!-- <li><a href="<?php //echo url_for('@newsletter')?>" >Recevoir une citation quotidienne par mail</a></li> -->
 	    </ul>
