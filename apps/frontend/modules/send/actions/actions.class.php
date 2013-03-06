f42f7ad4b9e2b71c5eefeb4db55887f24fdca737
Nathanaël Martel <nathanael@fam-martel.eu>
2013-03-06 23:05:34 +0100

    piwik goal

--- apps/frontend/modules/send/actions/actions.class.php
+++ apps/frontend/modules/send/actions/actions.class.php
@@ -65,7 +65,7 @@ class sendActions extends sfActions
             	$piwikTracker = new PiwikTracker( $idSite = 17 );
             	$piwikTracker->doTrackPageView('Envoyer la citation par mail');
             	$piwikTracker->doTrackGoal($idGoal = 2, $revenue = 100);
-            	$piwikTracker>setCustomVariable( 1, 'email', $send->email_from );
+            	$piwikTracker->setCustomVariable( 1, 'email', $send->email_from );
             	$piwikTracker->setCustomVariable( 2, 'citation', $send->citation_id );
             }
             
