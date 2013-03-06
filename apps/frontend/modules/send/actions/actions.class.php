077ec65333af0d2334a95c814bd3c2de33e5c578
Nathanaël Martel <nathanael@fam-martel.eu>
2013-03-06 22:57:57 +0100

    piwik goal

--- apps/frontend/modules/send/actions/actions.class.php
+++ apps/frontend/modules/send/actions/actions.class.php
@@ -56,6 +56,17 @@ class sendActions extends sfActions
             $this->getMailer()->send($message);
 
             $this->getUser()->setFlash('confirmation', 'Votre message a bien été envoyé !');
+            
+            if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+            {
+            	require_once sfConfig::get('sf_lib_dir').'/vendor/piwik/PiwikTracker.php';
+            	PiwikTracker::$URL = 'http://piwik.fam-martel.eu/';
+            
+            	$piwikTracker = new PiwikTracker( $idSite = 17 );
+            	$piwikTracker->doTrackPageView($sf_context->getResponse()->getTitle());
+            	$piwikTracker->doTrackGoal($idGoal = 2, $revenue = 100);
+            }
+            
             $this->redirect('@citation_short?id='.$send->citation_id);
         }
     }
