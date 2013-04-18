afd05b4dd9b43f7b1c086bb10a6b2a74ce2d82b6
Nathanaël Martel <nathanael@fam-martel.eu>
2013-04-18 22:15:50 +0200

    404

---
+++ apps/frontend/modules/default/actions/actions.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    simplement-web.com
+ * @subpackage default
+ * @author     Nathanaël Martel <nathanael@fam-martel.eu>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class defaultActions extends sfActions
+{
+
+    public function executeError404(sfWebRequest $request)
+    {
+    }
+}
---
+++ apps/frontend/modules/default/templates/error404Success.php
@@ -0,0 +1,14 @@
+
+  
+	<div class="grid-container content">
+		<div class="section">
+			<section>
+	      <div class="grid-100 tablet-grid-100 mobile-grid-100">
+	        <h1>Page introuvable</h1>
+	        
+					<p>La page que vous recherchez n'existe pas. Vous pouvez vous rattraper sur une <a href="<?php echo url_for('@random') ?>">citation aléatoire</a>.</p>
+				</div>
+			
+			</section>
+		</div>
+	</div>
