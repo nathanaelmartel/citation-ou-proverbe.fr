fc7475db0e8db88b90b5e0325370f215e1493ae0
Nathanaël <nathanael@fam-martel.eu>
2013-09-02 15:52:38 +0200

    schema.org

--- apps/frontend/modules/author/templates/showSuccess.php
+++ apps/frontend/modules/author/templates/showSuccess.php
@@ -22,20 +22,20 @@
 <?php endforeach ?>
 </div>
 
-<div id="author-<?php echo $author->id ?>" class="grid-40">
+<div id="author-<?php echo $author->id ?>" class="grid-40" itemscope itemtype="http://schema.org/Person">
 	<div class="author-card">
     <?php if ($author->getDates() != '' ): ?>
-    	<span class="dates"><?php echo $author->getDates() ?></span>
+    	<span class="dates" itemtype="birthDate"><?php echo $author->getDates() ?></span>
 		<?php endif; ?>
-    <h2 class="author-name">
+    <h2 class="author-name" itemtype="name">
     	<a href="<?php echo url_for('@author?slug='.$author->slug) ?>">
     		<?php echo $author->name ?>
     	</a>
     </h2>
     <?php if ($author->has_thumbnail): ?>
-			<img src="<?php echo url_for('@portrait_image?author='.$author->slug.'&effect=contour&sf_format=jpg&authorb='.$author->slug) ?>" alt="" class="portrait" />
+			<img src="<?php echo url_for('@portrait_image?author='.$author->slug.'&effect=contour&sf_format=jpg&authorb='.$author->slug) ?>" alt="" class="portrait" itemtype="image" />
 		<?php endif; ?>
-		<p><?php echo $author->getDescription() ?></p>
+		<p itemtype="description"><?php echo $author->getDescription() ?></p>
     <p>
     <?php if ($author->getWikipediaUrl() != '' ): ?>
     	<a href="<?php echo $author->getWikipediaUrl() ?>" class="wikipedia" target="_blank" ><?php echo $author->name ?> sur wikipedia</a><br />
@@ -50,7 +50,7 @@
 				<?php if ($page == $citations->getPage()): ?>
 					<span><?php echo $page?></span>
 				<?php else: ?>
-					<a href="<?php echo url_for( '@author_page?slug='.$author->slug.'&page='.$page) ?>" title="Citations ou Proverbes de : <?php echo $author->name?>"><?php echo $page?></a>
+					<a href="<?php echo url_for( '@author_page?slug='.$author->slug.'&page='.$page) ?>" title="Citations ou Proverbes de : <?php echo $author->name?>" itemtype="url"><?php echo $page?></a>
 				<?php endif; ?>
 			<?php endforeach ?>
 		</div>    
