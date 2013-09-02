2b946ffc430bc3554ec9abb3d9bb476f827ce8fb
Nathanaël <nathanael@fam-martel.eu>
2013-09-02 15:45:53 +0200

    schema.org

--- apps/frontend/modules/author/templates/_card.php
+++ apps/frontend/modules/author/templates/_card.php
@@ -1,23 +1,23 @@
 
 
-  <div id="author-<?php echo $author->id ?>" class="author-card">
+  <div id="author-<?php echo $author->id ?>" class="author-card" itemscope itemtype="http://schema.org/Person">
     <?php if ($author->getDates() != '' ): ?>
-    	<span class="dates"><?php echo $author->getDates() ?></span>
+    	<span class="dates" itemtype="birthDate"><?php echo $author->getDates() ?></span>
 		<?php endif; ?>
-    <h2 class="author-name">
+    <h2 class="author-name" itemtype="name">
     	<a href="<?php echo url_for('@author?slug='.$author->slug) ?>">
     		<?php echo $author->name ?>
     	</a>
     </h2>
     <?php if ($author->has_thumbnail): ?>
-			<img src="<?php echo url_for('@portrait_image?author='.$author->slug.'&effect=contour&sf_format=jpg&authorb='.$author->slug) ?>" alt="" class="portrait" />
+			<img src="<?php echo url_for('@portrait_image?author='.$author->slug.'&effect=contour&sf_format=jpg&authorb='.$author->slug) ?>" alt="" class="portrait" itemtype="image" />
 		<?php endif; ?>
-    <p><?php echo $author->getShortDescription() ?></p>
+    <p itemtype="description"><?php echo $author->getShortDescription() ?></p>
     <p>
     <?php if ($author->getWikipediaUrl() != '' ): ?>
     	<a href="<?php echo $author->getWikipediaUrl() ?>" class="wikipedia" target="_blank" ><?php echo $author->name ?> sur wikipedia</a><br />
 		<?php endif; ?>
-	   	<a href="<?php echo url_for('@author?slug='.$author->slug) ?>" class="author-link">
+	   	<a href="<?php echo url_for('@author?slug='.$author->slug) ?>" class="author-link" itemtype="url">
 	    	Toutes les citations de <?php echo $author->name ?>
 	    </a>
     </p>
