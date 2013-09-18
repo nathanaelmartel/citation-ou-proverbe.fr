6055b912c252ff4915ffd983bc4f9006584bb876
Nathanaël <nathanael@fam-martel.eu>
2013-09-18 11:02:02 +0200

    Sources

--- apps/frontend/modules/citation/templates/_card.php
+++ apps/frontend/modules/citation/templates/_card.php
@@ -6,7 +6,7 @@
 	</a>
 	<p class="context">
 		<a href="<?php echo url_for('@author?slug='.$citation->Author->slug) ?>" class="author"><?php echo $citation->Author->name ?></a>
-		<?php if ($citation->source_id != '' ): ?>
+		<?php if (($citation->source_id != '' ) && ($citation->Source->is_active)): ?>
 			<a href="<?php echo url_for('@source?slug='.$citation->Source->slug.'&author='.$citation->Author->slug ) ?>" target="_blank" ><?php echo $citation->getSource() ?></a>
 		<?php else: ?>
 			<span class="source"><?php echo $citation->getSource() ?></span>
