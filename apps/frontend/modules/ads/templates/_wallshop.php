3479276b7d8dd6df002aef51c6d1b7adf10e5e27
Nathanaël <nathanael@fam-martel.eu>
2014-12-03 19:33:49 +0100

    wallshop ads

---
+++ apps/frontend/modules/ads/templates/_wallshop.php
@@ -0,0 +1,25 @@
+<?php 
+
+	$baseline = array( 
+		'Affichez vos citations préférées !',
+		'Une belle phrase dans sur un beau tableau…',	
+		'Des mots pour décorer vos murs',	
+		'Tout part des mots',	
+		'Citations illustrées pour un intérieur bien décoré !',	
+		'Un tableau pour votre décoration ? Mettez-y une citation !',	
+	);
+
+?>
+
+
+<div class="clear"></div>
+
+<div class="ads ads-wallshop">
+	<h2><?php echo $baseline[rand(0, count($baseline)-1)]?></h2>
+	<p></p>
+	<p class="buttons">
+		<a href="http://Wallshop.fr/?utm_source=citation-ou-proverbe.fr&utm_medium=website&utm_campaign=citation-ou-proverbe.fr" class="button" target="_blank">Wallshop.fr</a> 
+	</p>
+</div>
+
+<div class="clear"></div>
